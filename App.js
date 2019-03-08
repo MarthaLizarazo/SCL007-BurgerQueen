a8f8f1f1cf8467945d50a47742d6124f10416937
MarthaLizarazo <johanalizarazo_8@hotmail.com>
2019-03-08 20:37:53 -0300

    haciendo npm install

--- App.js
+++ App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Bienvenido a React
           </a>
         </header>
       </div>
