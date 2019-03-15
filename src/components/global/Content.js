9f0fc63a8e9d8b7bb4a1057034714d750e2e4298
MarthaLizarazo <johanalizarazo_8@hotmail.com>
2019-03-15 15:12:57 -0300

    haciendo los menus

--- src/components/global/Content.js
+++ src/components/global/Content.js
@@ -3,21 +3,39 @@ import React, { Component } from 'react';
 // Components
 import ButtonDinner from'../global/ButtonDinner';
 import OrderClient from'../global/OrderClient';
+import {desayunos, comida} from '../global/Menus.json';
 import './css/Content.css';
 
 class Content extends Component {
+  constructor(props){
+  super(props);
+   this.state={
+     value:'',
+     desayunos,
+     comida,
+     drawDinner: false,
+     drawBreakfast:false,
+   };
+   this.drawDinner=this.drawDinner.bind(this);
+   this.drawBreakfast =this.drawBreakfast.bind(this);
+  }
+  drawDinner(){
+    this.setState({
+      drawDinner: !this.state.drawDinner
+    })
+  }
+  drawBreakfast(){
+    this.setState({
+      drawBreakfast: !this.state.drawBreakfast
+    })
+  }
   render() {
     return (
       <div className="Content">
             <OrderClient />
-            <div className="btn-group-vertical">
-            <p>
-              <button className="btn btn-danger btn-lg mr-5 m-4 p-3" onClick={this.onClick}>DESAYUNO</button>
-              <button className="btn btn-danger btn-lg mr-5 m-4 p-3" onClick={this.doClick}>ALMUERZO-CENA</button>
+              <button className="btn btn-danger btn-lg mr-5 m-4 p-3" onClick={this.drawDinner}>DESAYUNO</button>
+              <button className="btn btn-danger btn-lg mr-5 m-4 p-3" onClick={this.drawBreakfast}>ALMUERZO-CENA</button>
               <h4 className="float-right mr-5">**** TU PEDIDO ****</h4>
-            <ButtonDinner />
-            </p>
-            </div>
         </div>
     );
   }
