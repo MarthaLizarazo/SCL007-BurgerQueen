8eaa3cf0dee583c94d7b662ca09b097945dca23a
MarthaLizarazo <johanalizarazo_8@hotmail.com>
2019-03-15 21:21:13 -0300

    conectando con firebase

--- src/components/Firebase.js
+++ src/components/Firebase.js
@@ -1,17 +1,14 @@
-import Firebase from '../components/Firebase.js';
+import * as firebase from 'firebase';
 
-<script src="https://www.gstatic.com/firebasejs/5.9.0/firebase.js"></script>
-<script>
-  // Initialize Firebase
-  var config = {
+{/* <script src="https://www.gstatic.com/firebasejs/5.9.0/firebase.js"></script> */}
+  firebase.initializeApp({
     apiKey: "AIzaSyBr0Vqz631kSf8_mBzunPwILv3DNHD4nvo",
     authDomain: "burguerqueen-fb95e.firebaseapp.com",
     databaseURL: "https://burguerqueen-fb95e.firebaseio.com",
     projectId: "burguerqueen-fb95e",
     storageBucket: "burguerqueen-fb95e.appspot.com",
     messagingSenderId: "1003146480585"
-  };
-  firebase.initializeApp(config);
-</script>
+  });
+  let db = firebase.firestore();
 
-export default firestore;
+export default db;
